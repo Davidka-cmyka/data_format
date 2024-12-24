02da47e23542505972dbe3f840a082287605cb66
Davidka-cmyka <Daleksian@gmail.com>
2024-12-24 18:49:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import yaml
+import csv
+
+with open('data/gitlab-ci.yml', 'r') as file:
+    data = yaml.safe_load(file)
+
+stage_counts = {}
+
+for job_name, job_details in data.items():
+    if isinstance(job_details, dict) and 'stage' in job_details:
+        stage = job_details['stage']
+        if stage in stage_counts:
+            stage_counts[stage] += 1
+        else:
+            stage_counts[stage] = 1
+
+with open('stages_count.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(['Stage', 'Count'])
+    for stage, count in stage_counts.items():
+        writer.writerow([stage, count])
+
+print("CSV файл успешно обновлен с подсчетом stage.")
